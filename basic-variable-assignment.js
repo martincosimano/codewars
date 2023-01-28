871e3c4c94bd44743fbfb7f6c52da9dfab7a7485
Martin Cosimano <martincosimano@hotmail.com>
2023-01-28 20:44:52 -0300

    added codewars daily challenge

---
+++ basic-variable-assignment.js
@@ -0,0 +1,8 @@
+// This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+// var a == "code";
+// var b == "wa.rs";
+// var name == a + b;
+// My solution:
+let a = "code";
+let b = "wa.rs";
+let name = a + b;
