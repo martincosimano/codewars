5853dab5f423f32ef489ae29b2e14eb0aaba4a47
Martin Cosimano <martincosimano@hotmail.com>
2023-02-02 14:31:36 -0300

    added codewars daily challenge

---
+++ grasshopper-messi-goals-function.js
@@ -0,0 +1,12 @@
+// Complete the function to return his total number of goals in all three leagues.
+
+// Note: the input will always be valid.
+
+// For example:
+
+// 5, 10, 2  -->  17
+
+// My solution:
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
