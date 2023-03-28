2f26e31139d2b38cc61925b9519f806df3f42dc1
Martin Cosimano <martincosimano@hotmail.com>
2023-03-28 19:09:28 -0300

    added codewars daily challenge

---
+++ push-an-object-into-array.js
@@ -0,0 +1,7 @@
+// You are trying to put an object in javascript or java into an array, but it always returns error, solve it and keep it as simple as possible!
+// items = []
+// items.push {a: "b", c: "d"}
+
+// My solution:
+items = [];
+items.push ({a: "b", c: "d"});
