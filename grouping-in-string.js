16bb6cb63b0e0623dc18867785fb3ae7e9ee2bc6
martincosimano <martincosimano@hotmail.com>
2023-04-27 17:10:22 -0300

    added codewars daily challenge

---
+++ grouping-in-string.js
@@ -0,0 +1,18 @@
+// In this kata you have to find whether same elements of the string are grouped together.
+// Example
+
+// Input 	Output
+// 112233 	true
+// 11223311 	false
+
+// Explanation
+
+// In first example same elements are grouped together: (11)(22)(33), hence it should return true
+// In the second example there are two different groups of 1's: (11)2233(11), hence it should return false
+
+
+// My solution:
+const isConsecutive=s=> {
+    let arr = s.match(/(.)\1*/g);
+    return arr.length == new Set(arr).size;
+}
