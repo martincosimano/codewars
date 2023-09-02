13b2e90942f6d41e2e70c5e232b681d8888bbfab
Martin Cosimano <martincosimanodev@gmail.com>
2023-09-02 11:53:49 -0300

    added codewars daily challenge

---
+++ esreveR-gnirtS.js
@@ -0,0 +1,9 @@
+// Create a function called reverse for the String prototype that will allow the following functionality:
+
+// "String".reverse();// => returns "gnirtS"
+// "Super awesome string".reverse();// => returns "gnirts emosewa repuS"
+
+// My solution:
+String.prototype.reverse = function() {
+    return this.split('').reverse().join('');
+}
