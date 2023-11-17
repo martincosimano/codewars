d94b3b035c2a030065cf7229e24cc3365d38880e
Martin Cosimano <martincosimanodev@gmail.com>
2023-11-16 22:04:02 -0300

    added codewars daily challenge

---
+++ find-the-first-python-developer.js
@@ -0,0 +1,34 @@
+// You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising. The list is ordered according to who signed up first.
+
+// Your task is to return one of the following strings:
+
+//     < firstName here >, < country here > of the first Python developer who has signed up; or
+//     There will be no Python developers if no Python developer has signed up.
+
+// For example, given the following input array:
+
+// var list1 = [
+//   { firstName: 'Mark', lastName: 'G.', country: 'Scotland', continent: 'Europe', age: 22, language: 'JavaScript' },
+//   { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 30, language: 'Python' },
+//   { firstName: 'Emma', lastName: 'B.', country: 'Norway', continent: 'Europe', age: 19, language: 'Clojure' }
+// ];
+
+// your function should return Victoria, Puerto Rico.
+
+// Notes:
+
+//     The input array will always be valid and formatted as in the example above.
+
+// My solution:
+function getFirstPython(list) {
+    const pythonDevs = list.filter((obj) => obj.language === "Python");
+    return pythonDevs.length ? `${pythonDevs[0].firstName}, ${pythonDevs[0].country}` : "There will be no Python developers";
+}
+
+/*
+params: list as an array of objects
+ret: if there's a python dev, return its name and country, else return the provided string
+ 
+create a variable "pythonDevs" to store objects where language === "Python"
+if there's any, return the first dev name and country. Else return the provided string
+*/
